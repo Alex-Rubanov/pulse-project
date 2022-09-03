7ccfef9f6ea3ae02cd787dba565bf0b892d302bd
Alex <example@gmail.com>
2022-09-03 02:45:27 +0200

    add slider + content section (tab)

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/Slider/chevron_left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/Slider/chevron_right.svg"></button>'
+    });
+  });
+
+
