06a137bfe4f534f5973d4ef58b7fee4207c6c4ca
Alex <rubanovv.alex@gmail.com>
2022-09-06 03:10:06 +0200

    add map in footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/Slider/chevron_left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/Slider/chevron_right.svg"></button>'
+
     });
 
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
@@ -11,22 +11,6 @@ $(document).ready(function(){
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
       });
-
-    // $('.catalog-item__link').each(function(i) {
-    //     $(this).on('click', function(e) {
-    //         e.preventDefault();
-    //         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-    //         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-    //     })
-    // })
-
-    // $('.catalog-item__link-back').each(function(i) {
-    //     $(this).on('click', function(e) {
-    //         e.preventDefault();
-    //         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-    //         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-    //     })
-    // })
 
     function toggleSlide(item) {
         $(item).each(function(i) {
