c663a3efc0646dded36ff57aef0e1d677a5804a4
Alex <rubanovv.alex@gmail.com>
2022-09-09 02:39:46 +0200

    add modal + js

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
+    // Slider
     $('.carousel__inner').slick({
         speed: 1200,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/Slider/chevron_left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/Slider/chevron_right.svg"></button>'
 
     });
+
+    // Tabs
 
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
@@ -25,6 +28,28 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__link-back');
 
+    // Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function(){
+        $('.overlay, #consultation, #order, #feedback').fadeOut('slow');
+    });
+
+    $('.button_mini').each( function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        }) 
+    });
+
   });
+
+//   $(document).ready( function() {
+//     $('.gm-style').css('display','none');
+//     // find the name of the class using inspect element and then replace
+//     // it with the class name it will work 100%
+// });
 
 
